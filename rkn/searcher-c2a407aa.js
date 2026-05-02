0ca861d1d51b41778023a3e6bd6e2245456e986b
zapotek <tasos.laskos@gmail.com>
2026-05-02 23:24:38 +0300

    Update book

--- rkn/searcher-c2a407aa.js
+++ rkn/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f60f53e8.js',
+                path_to_root + 'searchindex-ba3eca74.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- scnr/searcher-c2a407aa.js
+++ scnr/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c0187e98.js',
+                path_to_root + 'searchindex-0f60916d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
