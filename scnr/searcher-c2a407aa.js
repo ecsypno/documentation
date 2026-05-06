9828150281bf680c1bf0aa07d43a52ef06463412
zapotek <tasos.laskos@gmail.com>
2026-05-06 16:32:24 +0300

    Update book

--- scnr/searcher-c2a407aa.js
+++ scnr/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-73a36ed1.js',
+                path_to_root + 'searchindex-ce203fc0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
