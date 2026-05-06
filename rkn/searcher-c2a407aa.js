3d94baba0f580a311fe4796e00dc850c4a307c3d
zapotek <tasos.laskos@gmail.com>
2026-05-06 16:18:12 +0300

    Update book

--- rkn/searcher-c2a407aa.js
+++ rkn/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f82e2683.js',
+                path_to_root + 'searchindex-975eaa73.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
