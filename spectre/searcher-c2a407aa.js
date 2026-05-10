a2f763ea1b0fd1c62bb7280e7e9ca6e3f10bbc11
zapotek <tasos.laskos@gmail.com>
2026-05-10 17:32:54 +0300

    Update book

--- spectre/searcher-c2a407aa.js
+++ spectre/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e812de1c.js',
+                path_to_root + 'searchindex-dfb952b5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
